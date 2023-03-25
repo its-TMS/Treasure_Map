e402f5d06a42b84440c1fd58752464b2b86030d7
Thoufeeq Md Sadique <110740296+its-TMS@users.noreply.github.com>
2023-03-25 21:26:25 +0530

    treasure_map.py

---
+++ treasure_map.py
@@ -0,0 +1,16 @@
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+
+map = [row1, row2, row3]
+
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Where do you want to put the treasure? ")
+
+y = int(position[0]) - 1
+x = int(position[1]) - 1
+
+map[x][y] = 'X'
+
+print(f"{row1}\n{row2}\n{row3}")
